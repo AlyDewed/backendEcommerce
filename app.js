9d81605ce8de4455f9296697bb07ca4945d3bd28
AlyDewed <alydewed@gmail.com>
2023-11-16 20:33:57 +0100

    Commit new version

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ mongoose.set('strictQuery', true)
 
 const connect = async () => {
 try {
-await mongoose.connect(process.env.DATABASE);
+await mongoose.connect(process.env.DATABASECLOUD);
 console.log("Connected to mongoDB.");
 } catch (error) {
 throw error;
@@ -34,3 +34,5 @@ app.use("/api/articles", articleRouter)
 app.listen(process.env.PORT, () => {
     connect();
 console.log(`Server is listening on port ${process.env.PORT}`); });
+
+module.exports = app;
